b756e20d31cd730212fe6f6a1890ba5517259eae
Lucas Gabriel <lucasdias200lucasdias@gmail.com>
2026-03-03 15:33:44 -0300

    feat: implement AuthController with login, logout, and register methods

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    function login(UserLoginRequest $request){
+       //TODO
+    }
+
+    function logout(Request $request){
+       //TODO
+    }
+
+    function register(UserStoreRequest $request){
+       //TODO
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/users', UserController::class)->whereNumber('user');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::apiResource('/users', UserController::class)->whereNumber('user')->withMiddleware('auth:sanctum');
